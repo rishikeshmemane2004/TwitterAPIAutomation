425fdf31bda9cb8d6a21ccb485dda34c9e03224d
rishikeshmemane2004 <53008391+rishikeshmemane2004@users.noreply.github.com>
2021-02-21 18:08:59 +0530

    Update Auth.java

--- src/test/java/com/onlyautomationtesting/constants/Auth.java
+++ src/test/java/com/onlyautomationtesting/constants/Auth.java
@@ -2,9 +2,9 @@ package com.onlyautomationtesting.constants;
 
 public class Auth {
 
-	public static final String CONSUMER_KEY     = "NKrmjUmkUlOtxv79D6aYrULi4";
-	public static final String CONSUMER_SECRET  = "ZCsixJd5oF9eOwifrIyDAWL61yOFODRapGsXQrxLysTRKrWkey"; 
-	public static final String ACCESS_TOKEN     = "1362374764417875975-ZDTHkXQgn9WOdFiaE7hBWQj8yPQNqp"; 
-	public static final String ACCESS_SECRET    = "MTgmuOvpCAEkpEy4bovrXgW3r2BkZKCMf4BBDtGXqlVwb";
+	public static final String CONSUMER_KEY     = "NKrmjUmkUlOtxv79D6aYrULi424";
+	public static final String CONSUMER_SECRET  = "98ZCsixJd5oF9eOwifrIyDAWL61yOFODRapGsXQrxLysTRKrWkey"; 
+	public static final String ACCESS_TOKEN     = "1362374764417875975-ZDTHkXQgn9WOdFiaE7hBWQj8yPQNqp24"; 
+	public static final String ACCESS_SECRET    = "98MTgmuOvpCAEkpEy4bovrXgW3r2BkZKCMf4BBDtGXqlVwb";
 	
 }
